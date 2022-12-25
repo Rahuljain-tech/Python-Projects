aeb509a496de9e25dd4cef70a7c5919292adfadd
Rahul Jain <jainraahul03@gmail.com>
2022-12-25 12:07:55 +0530

    Added main.py, utils.py, functions and requests .

---
+++ functions/os_ops.py
@@ -0,0 +1,32 @@
+import os
+import subprocess as sp
+
+paths = {
+    'notepad': "C:\\Users\\Mukesh\\AppData\\Local\\Microsoft\\WindowsApps\\notepad++.exe",
+    'discord': "C:\\Users\\ashut\\AppData\\Local\\Discord\\app-1.0.9003\\Discord.exe",
+    'calculator': "C:\\Windows\\System32\\calc.exe",
+    'spotify': "C:\\Users\\Mukesh\\AppData\\Local\\Microsoft\\WindowsApps\\Spotify.exe"
+}
+
+
+def open_notepad():
+    os.startfile(paths['notepad'])
+
+
+def open_discord():
+    os.startfile(paths['discord'])
+
+
+def open_cmd():
+    os.system('start cmd')
+
+
+def open_camera():
+    sp.run('start microsoft.windows.camera:', shell=True)
+
+
+def open_calculator():
+    sp.Popen(paths['calculator'])
+
+def open_spotify():
+    sp.Popen(paths['spotify'])
